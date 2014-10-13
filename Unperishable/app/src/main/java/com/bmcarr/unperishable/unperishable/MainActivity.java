154a5a75b318a3517b7bfb2047b47c8c23492e0f
Brett Carr <bmcarr928@gmail.com>
2014-10-12 17:45:46 -0700

    Added a comment

--- Unperishable/app/src/main/java/com/bmcarr/unperishable/unperishable/MainActivity.java
+++ Unperishable/app/src/main/java/com/bmcarr/unperishable/unperishable/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        // Comment!
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
