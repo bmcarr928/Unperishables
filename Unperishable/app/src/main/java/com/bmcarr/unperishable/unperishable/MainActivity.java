25546c4d12d3e3542a096869d344d042a79a6a1d
Brett Carr <bmcarr928@gmail.com>
2014-10-12 18:03:12 -0700

    Testing

--- Unperishable/app/src/main/java/com/bmcarr/unperishable/unperishable/MainActivity.java
+++ Unperishable/app/src/main/java/com/bmcarr/unperishable/unperishable/MainActivity.java
@@ -25,10 +25,10 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle cliks on the Home/Up button, so long
+        // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings) {g
             return true;
         }
         return super.onOptionsItemSelected(item);
