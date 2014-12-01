22bfb17624722b3dc9867fd5038d433eaa8744f0
bmcarr928 <bmcarr928@gmail.com>
2014-12-01 04:51:05 -0700

    Basic server functionality working

--- Unperishable-web/api/api_functions.py
+++ Unperishable-web/api/api_functions.py
@@ -1,60 +1,57 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from api.models import Item, User
+from django.core import serializers
 import json
 import time
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hellllllo");
+    return HttpResponse("{\"word\":\"hello\"}");
 
+@csrf_exempt
 def additem(request):
-    if request.GET:
+    if request.method == 'GET':
+        Item.objects.all().delete()
+        User.objects.all().delete()
+        u1 = User.objects.get_or_create(name="Bob", password="bobness")[0]
+        u2 = User.objects.get_or_create(name="Steve", password="steveness")[0]
+        p_add_item("bananas", 0, 2, master=u1)
+        p_add_item("milk", 0, 1, master=u1)
+        p_add_item("cheese", 0, 2, master=u1)
+        p_add_item("salt", 2, 2, master=u2)
+        p_add_item("pepper", 2, 0, master=u2)
+        p_add_item("green beans", 1, 2, master=u2)
         return HttpResponse("Added an item")
     else:
+        string = request.read()
+        json_request = json.loads(string)
+        json_owner = json_request.get('name')
+        json_item = json_request.get('item')
+        name = json_item['name']
+        category = json_item['category']
+        quantity = json_item['quantity']
+        expiration_date = json_item.get('input_date', current_time_millis())
+        input_date = json_item.get('expiration_date', current_time_millis())
+        user = User.objects.get(name=json_owner)
+        p_add_item(name=name, category=category, quantity=quantity, master=user,
+                   input_date=input_date, expiration_date=expiration_date, owner=user)
         return HttpResponse("What?")
 
-
+@csrf_exempt
 def sync(request):
-    json_array = []
-    json_item1 = {
-        "name":"bananas",
-        "category":0,
-        "quantity":1,
-        "input_date":current_time_millis(),
-        "expiration_date":current_time_millis(),
-        "owner":""
-    }
-    json_item2 = {
-        "name":"bananas",
-        "category":0,
-        "quantity":1,
-        "input_date":current_time_millis(),
-        "expiration_date":current_time_millis(),
-        "owner":""
-    }
-    json_item3 = {
-        "name":"bananas",
-        "category":0,
-        "quantity":1,
-        "input_date":current_time_millis(),
-        "expiration_date":current_time_millis(),
-        "owner":""
-    }
-    json_item4 = {
-        "name":"bananas",
-        "category":0,
-        "quantity":1,
-        "input_date":current_time_millis(),
-        "expiration_date":current_time_millis(),
-        "owner":""
-    }
-    json_array.append(json_item1)
-    json_array.append(json_item2)
-    json_array.append(json_item3)
-    json_array.append(json_item4)
-    return HttpResponse(json.dumps(json_array));
-
-
+    string = request.read()
+    json_request = json.loads(string)
+    user = User.objects.filter(name=json_request['name'])
+    return HttpResponse(serializers.serialize("json", Item.objects.filter(master_account=user)))
 
 def current_time_millis():
     return int(round(time.time() * 1000))
+
+def p_add_item(name, category, quantity, master, input_date=current_time_millis(),
+               expiration_date=current_time_millis(), owner=""):
+    i = Item.objects.get_or_create(name=name, category=category, quantity=quantity,
+                                   input_date=input_date, expiration_date=expiration_date,
+                                   owner=owner, master_account=master)[0]
+    return i
--- Unperishable-web/api/models.py
+++ Unperishable-web/api/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    password = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return self.name
+
 class Item(models.Model):
     name = models.CharField(max_length=128, unique=True)
     quantity = models.IntegerField(default=0)
@@ -7,14 +14,7 @@ class Item(models.Model):
     input_date = models.IntegerField(default=0)
     expiration_date = models.IntegerField(default=0)
     owner = models.CharField(max_length=128)
+    master_account = models.ForeignKey('User')
 
     def __unicode__(self):
         return self.name
-
-class User(models.Model):
-    name = models.CharField(max_length=128)
-    password = models.CharField(max_length=128)
-    master_account = models.ForeignKey('self')
-
-    def __unicode__(self):
-        return self.title
