134c670f317415a114e6b397ea07c7dc6f08b960
Jeff Phelan <JeffPhelan@users.noreply.github.com>
2014-12-02 23:21:22 -0700

    Start of login fragment

--- Unperishable-Android/app/src/main/java/com/bmcarr/unperishable/view/LoginFragment.java
+++ Unperishable-Android/app/src/main/java/com/bmcarr/unperishable/view/LoginFragment.java
@@ -2,11 +2,14 @@ package com.bmcarr.unperishable.view;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.bmcarr.unperishable.R;
 
@@ -63,16 +66,28 @@ public class LoginFragment extends Fragment {
         this.passwordField = (EditText) view.findViewById(R.id.txtUsername);
 
         Button loginButton = (Button) view.findViewById(R.id.login_button);
-
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
+                if(!emptyFields()){
+                  if (existingUser()) {
+
+                  } else {
+                      Toast toast= Toast.makeText(getActivity(), "User already exist!", Toast.LENGTH_SHORT);
+                      toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, -50);
+                      toast.show();
+                  }
+                }
+
                 ((MainActivity)LoginFragment.this.getActivity()).loginUser(LoginFragment.this.usernameField.getText().toString());
+
+
             }
         });
 
         Button createAccountButton = (Button) view.findViewById(R.id.create_account_button);
-
         createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -86,5 +101,50 @@ public class LoginFragment extends Fragment {
         return view;
     }
 
+    private boolean emptyFields() {
+        /* Test if the username field is empty */
+        if(TextUtils.isEmpty(username.getText().toString())) {
+            username.setError("Please enter a username");
+            username.requestFocus();
+            return true;
+        }
+        /* Test if the password field is empty */
+        if(TextUtils.isEmpty(psw.getText().toString())) {
+            psw.setError("Please enter a password");
+            psw.requestFocus();
+            return true;
+        }
+        /* Test if the password confirmation field is empty */
+        if(TextUtils.isEmpty(cpsw.getText().toString())) {
+            cpsw.setError("Please confirm password");
+            cpsw.requestFocus();
+            return true;
+        }
+        /* Test if the email field is empty */
+        if(TextUtils.isEmpty(email.getText().toString())) {
+            email.setError("Please enter an email");
+            email.requestFocus();
+            return true;
+        }
+        /* Test if the email confirmation field is empty */
+        if(TextUtils.isEmpty(cemail.getText().toString())) {
+            cemail.setError("Please confirm email");
+            cemail.requestFocus();
+            return true;
+        }
+        return false;
+    }
+    private boolean existingUser() {
+        boolean userExist = true;
+
+        // TODO: Check if user exist here
+
+
+        if(userExist) {
+            usernameField.requestFocus();
+            return true;
+        }
+        return false;
+    }
 
 }
