2070621d289332f47bcada1299e6d4335653e0b4
bmcarr928 <bmcarr928@gmail.com>
2014-12-02 21:36:32 -0700

    Basic api functions working now

--- Unperishable-web/api/api_functions.py
+++ Unperishable-web/api/api_functions.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from api.models import Item, User
@@ -8,36 +9,79 @@ import time
 
 # Create your views here.
 def index(request):
-    return HttpResponse("{\"word\":\"hello\"}");
+    return HttpResponse("{\"word\":\"hello\"}")
+
+@csrf_exempt
+def adduser(request):
+    json_request = json.loads(request.read())
+    username = json_request['name']
+    password = json_request['password']
+    try:
+        User.objects.create(name=username, password=password)
+        return HttpResponse("Success")
+    except IntegrityError:
+        return HttpResponse("Failed", status=403)
+
+@csrf_exempt
+def loginuser(request):
+    json_request = json.loads(request.read())
+    username = json_request['name']
+    password = json_request['password']
+    try:
+        User.objects.get(name=username, password=password)
+        return HttpResponse("Success")
+    except ObjectDoesNotExist:
+        return HttpResponse("Failed", status=403)
 
 @csrf_exempt
 def additem(request):
-    if request.method == 'GET':
-        Item.objects.all().delete()
-        User.objects.all().delete()
-        u1 = User.objects.get_or_create(name="Bob", password="bobness")[0]
-        u2 = User.objects.get_or_create(name="Steve", password="steveness")[0]
-        p_add_item("bananas", 0, 2, master=u1)
-        p_add_item("milk", 0, 1, master=u1)
-        p_add_item("cheese", 0, 2, master=u1)
-        p_add_item("salt", 2, 2, master=u2)
-        p_add_item("pepper", 2, 0, master=u2)
-        p_add_item("green beans", 1, 2, master=u2)
-        return HttpResponse("Added an item")
-    else:
-        string = request.read()
-        json_request = json.loads(string)
-        json_owner = json_request.get('name')
-        json_item = json_request.get('item')
-        name = json_item['name']
-        category = json_item['category']
-        quantity = json_item['quantity']
-        expiration_date = json_item.get('input_date', current_time_millis())
-        input_date = json_item.get('expiration_date', current_time_millis())
-        user = User.objects.get(name=json_owner)
+    string = request.read()
+    json_request = json.loads(string)
+    json_owner = json_request.get('name')
+    json_item = json_request.get('item')
+    name = json_item['name']
+    category = json_item['category']
+    quantity = json_item['quantity']
+    expiration_date = json_item.get('input_date', current_time_millis())
+    input_date = json_item.get('expiration_date', current_time_millis())
+    user = User.objects.get(name=json_owner)
+    try:
         p_add_item(name=name, category=category, quantity=quantity, master=user,
                    input_date=input_date, expiration_date=expiration_date, owner=user)
-        return HttpResponse("What?")
+        return HttpResponse("Success")
+    except IntegrityError:
+        return HttpResponse("Failed", status=403)
+
+@csrf_exempt
+def updateitem(request):
+    string = request.read()
+    json_request = json.loads(string)
+    json_owner = json_request.get('name')
+    json_item = json_request.get('item')
+    name = json_item['name']
+    category = json_item['category']
+    quantity = json_item['quantity']
+    expiration_date = json_item.get('input_date', current_time_millis())
+    input_date = json_item.get('expiration_date', current_time_millis())
+    user = User.objects.get(name=json_owner)
+    try:
+        p_update_item(name=name, category=category, quantity=quantity, master=user,
+                      input_date=input_date, expiration_date=expiration_date, owner=user.name)
+        return HttpResponse("Success")
+    except ObjectDoesNotExist:
+        return HttpResponse("Failed, object does not exist", status=403)
+
+@csrf_exempt
+def deleteitem(request):
+    string = request.read()
+    json_request = json.loads(string)
+    json_owner = json_request.get('name')
+    item_name = json_request.get('item_name')
+    try:
+        Item.objects.filter(name=item_name, owner=json_owner).delete()
+        return HttpResponse("Success")
+    except ObjectDoesNotExist:
+        return HttpResponse("Failed, object does not exist", status=403)
 
 @csrf_exempt
 def sync(request):
@@ -49,9 +93,23 @@ def sync(request):
 def current_time_millis():
     return int(round(time.time() * 1000))
 
+def p_update_item(name, category, quantity, master, input_date=current_time_millis(),
+                  expiration_date=current_time_millis(), owner=""):
+    try:
+        i = Item.objects.filter(name=name, master_account=master)\
+                        .update(category=category, quantity=quantity,
+                                input_date=input_date, expiration_date=expiration_date,
+                                owner=owner)
+        return i
+    except ObjectDoesNotExist:
+        raise ObjectDoesNotExist
+
 def p_add_item(name, category, quantity, master, input_date=current_time_millis(),
                expiration_date=current_time_millis(), owner=""):
-    i = Item.objects.get_or_create(name=name, category=category, quantity=quantity,
-                                   input_date=input_date, expiration_date=expiration_date,
-                                   owner=owner, master_account=master)[0]
-    return i
+    try:
+        i = Item.objects.create(name=name, category=category, quantity=quantity,
+                                input_date=input_date, expiration_date=expiration_date,
+                                owner=owner, master_account=master)
+        return i
+    except IntegrityError:
+        raise IntegrityError
--- Unperishable-web/api/urls.py
+++ Unperishable-web/api/urls.py
@@ -3,6 +3,10 @@ from api import views, api_functions
 
 urlpatterns = patterns('',
         url(r'^$', api_functions.index, name='index'),
+        url(r'^adduser', api_functions.adduser, name='adduser'),
+        url(r'^loginuser', api_functions.loginuser, name='loginuser'),
         url(r'^additem', api_functions.additem, name='additem'),
+        url(r'^updateitem', api_functions.updateitem, name='updateitem'),
+        url(r'^deleteitem', api_functions.deleteitem, name='deleteitem'),
         url(r'^sync', api_functions.sync, name='sync'),
         )
